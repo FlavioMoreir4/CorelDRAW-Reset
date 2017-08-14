f575cddb3c56fea9325f5d2fcd30ecb37b02f36a
Flavio Moreira <flavio.moreira@outlook.com>
2017-08-14 15:01:32 -0300

    CorelDRAW Reset

---
+++ CorelDRAWReset/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StartCorel = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartCorel
+            // 
+            this.StartCorel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.StartCorel.Location = new System.Drawing.Point(14, 75);
+            this.StartCorel.Margin = new System.Windows.Forms.Padding(5);
+            this.StartCorel.Name = "StartCorel";
+            this.StartCorel.Size = new System.Drawing.Size(168, 51);
+            this.StartCorel.TabIndex = 3;
+            this.StartCorel.Text = "Abrir CorelDRAW";
+            this.StartCorel.UseVisualStyleBackColor = true;
+            this.StartCorel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Reset.Location = new System.Drawing.Point(14, 14);
+            this.Reset.Margin = new System.Windows.Forms.Padding(5);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(168, 51);
+            this.Reset.TabIndex = 2;
+            this.Reset.Text = "Resetar";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Provided by Flavio Moreira";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 156);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartCorel);
+            this.Controls.Add(this.Reset);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(214, 195);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(214, 195);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CorelDRAW Reset";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartCorel;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CorelDRAWReset/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+
+            System.Diagnostics.Process ResetCorel = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo ResetCorelInfo = new System.Diagnostics.ProcessStartInfo();
+
+            ResetCorelInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            ResetCorelInfo.FileName = "REG.exe";
+            ResetCorelInfo.Arguments = "DELETE HKCU\\Software\\Corel /f";
+
+            ResetCorel.StartInfo = ResetCorelInfo;
+            ResetCorel.Start();
+
+            DialogResult StartCorel = MessageBox.Show("Deseja iniciar o CorelDRAW?", "Reset concluído", MessageBoxButtons.YesNo);
+
+            if (StartCorel == DialogResult.Yes)
+            {
+                System.Diagnostics.Process CorelDRW = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo CorelDRWInfo = new System.Diagnostics.ProcessStartInfo();
+
+                CorelDRWInfo.FileName = "CorelDRW";
+
+                CorelDRW.StartInfo = CorelDRWInfo;
+                CorelDRW.Start();
+                Application.Exit();
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process CorelDRW = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo CorelDRWInfo = new System.Diagnostics.ProcessStartInfo();
+
+            CorelDRWInfo.FileName = "CorelDRW";
+
+            CorelDRW.StartInfo = CorelDRWInfo;
+            CorelDRW.Start();
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://flaviomoreira.16mb.com");
+        }
+
+    }
+}
